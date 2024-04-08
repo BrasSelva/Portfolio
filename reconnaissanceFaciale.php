a4341fa35b83c5dc98622da5835d46f19f6e4cba
brassica <brassica943@gmail.com>
2024-04-08 15:34:29 +0200

    first commit

---
+++ reconnaissanceFaciale.php
@@ -0,0 +1,88 @@
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="satellite.css">
+
+<!------ Include the above in your HEAD tag ---------->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Poppins:400,500,600,700" rel="stylesheet"> 
+<br/>
+<center><h4>L'évolution de la reconnaissance faciale au cours du temps:</h4></center>
+
+
+<section class="experience pt-100 pb-100" id="experience">
+		<div class="container">
+			
+			<div class="row">
+               <div class="col-xl-12">
+                  <ul class="timeline-list">
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                           <span>1991</span>
+                           <h4>Premier exemple réussi de technologie de reconnaissance faciale</h4>
+                           <img src="assets/img/visage.png" alt="">
+                           <p>Le projet de Eigenfaces permet de faire une reconnaissance faciale, en employant des photos de référence des personnes connues et en soumettant au programme la photo d'une personne à identifier.</p>
+                           <a href="https://silanus.fr/sin/?p=785" class="btn btn-danger">Lire</a>
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>2018</span>
+                           <h4>La police chinoise arrête un suspect grâce à l’utilisation de la reconnaissance faciale</h4>
+                           <img src="assets/img/arrestation.png" alt="">
+                           <p>Le visage du suspect était répertorié dans une base de données nationale, et a été repéré dans une foule de 50 000 personnes. Les technologies de reconnaissance faciales sont de plus en plus présentes en Chine.</p>
+                           <a href="https://www.journaldugeek.com/2018/04/13/reconnaissance-faciale-chinois-arrete-apres-ete-repere-milieu-dune-foule-de-50-000-personnes-a-concert/" class="btn btn-danger">Lire</a>
+
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>Février 2019</span>
+                           <h4>Nice teste la reconnaissance faciale lors d'un Carnaval</h4>
+                           <img src="assets/img/video.jpg" alt="">
+                           <p>Cette experience permettra  de rechercher parmi les participants des personnes disparues ou suspectes. Les personnes qui ne seront pas volontaires auront le visage flouté et ne seront pas reconnaissables sur les images.</p>
+                           <a href="https://www.lepoint.fr/societe/nice-va-tester-la-reconnaissance-faciale-lors-du-carnaval-19-02-2019-2294405_23.php" class="btn btn-danger">Lire</a>
+
+                        </div>
+                     </li>
+
+                        <!-- Single Experience -->
+                        <li>
+                        <div class="timeline_content">
+                        <span>juin 2019</span>
+                           <h4>Alicem, la première solution d’identité numérique sécurisée</h4>
+                           <img src="assets/img/alicem.jpg" alt="">
+                           <p>Pouvoir prouver son identité en ligne, de façon simple et sécurisée, en évitant les risques d’usurpation et de détournement de ses données personnelles, est devenu indispensable.</p>
+                           <a href="https://www.interieur.gouv.fr/actualites/actu-du-ministere/alicem-premiere-solution-didentite-numerique-regalienne-securisee" class="btn btn-danger">Lire</a>
+
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>2020</span>
+                           <h4>En Chine, la reconnaissance faciale est désormais obligatoire avant d’acheter un smartphone</h4>
+                           <img src="assets/img/smartphone.jpg" alt="">
+                           <p>La loi contraint désormais toutes les personnes qui achètent un nouveau smartphone dans une boutique d'enregistrer leur visage par un logiciel de reconnaissance faciale.  </p>
+                           <a href="https://www.phonandroid.com/chine-reconnaissance-faciale-obligatoire-acheter-smartphone.html" class="btn btn-danger">Lire</a>
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>Future événement "2024"</span>
+                           <h4> Le Parlement français dit oui à la surveillance par IA pour les JO de Paris 2024 </h4>
+                           <img src="assets/img/serveur.jpg" alt="">
+                           <p>il autorise une telle surveillance à titre expérimental, dans le seul but d'assurer la sécurité des manifestations sportives, récréatives ou culturelles qui sont particulièrement exposées au risque d'actes de terrorisme ou de menaces graves pour la sécurité des personnes. </p>
+                           <a href="https://www.theregister.com/2023/03/24/al_surveillance_french/" class="btn btn-danger">Lire</a>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+		</div>
+	  </section>
+     
---
+++ satellite.php
@@ -0,0 +1,89 @@
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="satellite.css">
+
+<!------ Include the above in your HEAD tag ---------->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Poppins:400,500,600,700" rel="stylesheet"> 
+<br/>
+<center><h4>L'évolution des satellites au cours du temps:</h4></center>
+
+
+<section class="experience pt-100 pb-100" id="experience">
+		<div class="container">
+			
+			<div class="row">
+               <div class="col-xl-12">
+                  <ul class="timeline-list">
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                           <span>1960-1984</span>
+                           <h4>Course aux satellites</h4>
+                           <img src="assets/img/satellite1.jpg" alt="">
+                           <p>La conquête de l’espace, les premiers satellites de télécommunications font leurs apparitions ce qui permit de réaliser les premières transmissions d’images de télévision.</p>
+                           <a href="https://www.cite-telecoms.com/blog/histoire/200-ans-de-telecoms/les-temps-modernes/les-premiers-satellites/#:~:text=Le%20premier%20v%C3%A9ritable%20satellite%2C%20Courrier,une%20r%C3%A9ussite%20beaucoup%20plus%20probante" class="btn btn-danger">Lire</a>
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>2000-2010</span>
+                           <h4>La miniaturisation des satellites par "New space"</h4>
+                           <img src="assets/img/newSpace.jpg" alt="">
+                           <p>Aujourd’hui, la tendance est la miniaturisation des satellites. Une opération qui consiste à réduire la masse et l’énergie consommée des satellites artificiels. L’objectif de cette réduction de poids est de réduire les coûts de lancement, qui constituent un poste budgétaire très important.</p>
+                           <a href="https://surfeo.fr/la-miniaturisation-des-satellites-et-des-lanceurs-avenement-du-new-space-low-cost/" class="btn btn-danger">Lire</a>
+
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>2019</span>
+                           <h4>L’objectif de SpaceX</h4>
+                           <img src="assets/img/Elon1.jpg" alt="">
+                           <p>SpaceX a demandé à l’Union internationale des télécommunications l’autorisation de mettre 30000 satellites supplémentaires en orbite.
+                           Cela permettra de fournir un accès Internet à haute vitesse et à travers la planète Terre.</p>
+                           <a href="https://lte.ma/evolution-de-lhistoire-des-telecoms-par-satellite-dans-le-monde-de-1945-a-2020/" class="btn btn-danger">Lire</a>
+
+                        </div>
+                     </li>
+
+                        <!-- Single Experience -->
+                        <li>
+                        <div class="timeline_content">
+                        <span>2021</span>
+                           <h4>Un test d'intelligence artificiel embarqué à bord du satellite CUBESAT OPS-SAT</h4>
+                           <img src="assets/img/IA.jpg" alt="">
+                           <p>L’Intelligence Artificielle embarquée à bord de satellites est la porte d’entrée vers un champ d’applications exceptionnel. En matière de surveillance de l’environnement, cette nouvelle technologie permet de lever des alertes en temps réel en cas de départs de feux, de pollutions marines, et bien plus encore.</p>
+                           <a href="https://www.thalesgroup.com/fr/monde/espace/news/test-dintelligence-artificiel-mene-succes-bord-du-cubesat-ops-sat#:~:text=L%27Intelligence%20Artificielle%20embarqu%C3%A9e%20%C3%A0%20bord%20de%20satellites%20est%20la,marines%2C%20et%20bien%20plus%20encore" class="btn btn-danger">Lire</a>
+
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>2022</span>
+                           <h4>Le hack satellite de la Russie face à l'Ukraine</h4>
+                           <img src="assets/img/terre.jpg" alt="">
+                           <p>Un regard sur la cyberattaque de la Russie sur le réseau KA-SAT de Viasat, conduisant certains pays à débattre de la manière d'améliorer la sécurité des satellites. </p>
+                           <a href="https://www.bloomberg.com/features/2023-russia-viasat-hack-ukraine/?leadSource=uverify%20wall" class="btn btn-danger">Lire</a>
+                        </div>
+                     </li>
+                     <!-- Single Experience -->
+                     <li>
+                        <div class="timeline_content">
+                        <span>Future événement "2025"</span>
+                           <h4> Un réseau de satellites de télécommunications est en marche. </h4>
+                           <img src="assets/img/e1.jpg" alt="">
+                           <p>Les cyberattaques visant différentes infrastructures se multipliant, l’Europe entend renforcer rapidement la confidentialité de ses communications sensibles. </p>
+                           <a href="https://www.techniques-ingenieur.fr/actualite/articles/une-constellation-de-satellites-pour-la-securite-de-ses-communications-en-europe-121366/" class="btn btn-danger">Lire</a>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+		</div>
+	  </section>
+     
